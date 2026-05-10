e048ec01512f61138b1fc28708fd6e7b0c9ca40a
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2026-05-10 17:48:37 -0300

    feat: creates basic agent file

---
+++ media_content_generator_agent/agent.py
@@ -0,0 +1,39 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.tavily import TavilyTools
+from agno.db.sqlite import SqliteDb
+from agno.os import AgentOS
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Setup DB
+db = SqliteDb(db_file="tmp/data.db")
+
+# Setup Agents
+copywriter = Agent(
+    model=OpenAIChat(id="gpt-5-nano"),
+    name="copywriter",
+    description="",
+
+
+    add_history_to_context=True,
+    num_history_runs=3,
+
+    tools=[TavilyTools()],
+    db = db,
+    instructions=open("prompts/copywriter.md").read()
+)
+
+# Setup AgentOs
+
+agent_os = AgentOS(
+    name="agent_os",
+    description="",
+    agents=[copywriter]
+    )
+
+app = agent_os.get_app()
+
+if __name__ == "__main__":
+    agent_os.serve("agent.py:app", reload=True)
