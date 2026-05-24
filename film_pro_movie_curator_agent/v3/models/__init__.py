254d35782c0e09d6d9f1f815c4c05fd4531f2b8b
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2026-05-24 17:55:35 -0300

    feat: creates v3 with access to omdb api through custom tool

---
+++ film_pro_movie_curator_agent/v3/models/__init__.py
@@ -0,0 +1,8 @@
+from .models import MovieRecommendation, Movie, Cast
+
+# if someone uses * python will only import the exact name listed in the array
+___all___ = [
+    "MovieRecommendation",
+    "Movie",
+    "Cast"
+]
