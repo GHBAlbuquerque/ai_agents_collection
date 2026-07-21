76fe383fc8759bcbaa2c56d92d817830d7e1452d
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2026-07-20 22:01:10 -0300

    chore: config updates

--- RO_character_lore_generator/configs.py
+++ RO_character_lore_generator/configs.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 FILES_FOLDER = Path(__file__).parent / 'files'
+EXAMPLES_FOLDER = Path(__file__).parent / 'examples'
---
+++ RO_character_lore_generator/prompt/__init__.py
@@ -0,0 +1,5 @@
+from .system_prompt import SYSTEM_PROMPT
+
+__all__ = [
+    "SYSTEM_PROMPT"
+]
