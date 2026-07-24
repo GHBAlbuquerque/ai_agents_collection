390b69344d3c6d216a60387b4f73ef2d80a46753
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2026-07-23 21:58:26 -0300

    feat: creates file ingestion related functions

--- RO_character_lore_generator/configs.py
+++ RO_character_lore_generator/configs.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 FILES_FOLDER = Path(__file__).parent / 'files'
 EXAMPLES_FOLDER = Path(__file__).parent / 'examples'
+VECTOR_DB_FOLDER = Path(__file__).parent / 'vector_db'
+EMBEDDING_MODEL = "text-embedding-ada-002"
+COLLECTION_NAME = "RO_LORE_COMPEDIUM"
